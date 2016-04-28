497d8fbf8924b63d451b82256f2abbf4f0406ebb
Linus Heimböck <linus.heimboeck@live.at>
2016-04-28 20:58:29 +0200

    asdf

--- AdaLollA_LinuxH_android/DummyProject/app/src/main/java/com/gghhr/dummyproject/MainActivity.java
+++ AdaLollA_LinuxH_android/DummyProject/app/src/main/java/com/gghhr/dummyproject/MainActivity.java
@@ -16,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     public void thisIsANewMethod() {
 		Log.d("debug", "called method");
     	int i = 0;
+    	for (int i = 0; i < 10000; i++) {
+    		
+    	}
     }
 
 }
